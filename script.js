7e9bd4a89d5ee6213e480e85ae037a2c98fd047f
Srinath08-sri <johnsrinath45@gmail.com>
2026-01-14 13:36:55 +0530

    JS

---
+++ script.js
@@ -0,0 +1,24 @@
+// name='sri'
+// id=12
+// collegename='SFS'
+// address='hosur'
+// age=0x12
+// qualification='Bsc'
+// console.log("name:",name)
+// console.log("id:",id)
+// console.log("collegename:",collegename)
+// console.log("address:",address)
+// console.log("age:",age)
+// console.log("qualification:",qualification)
+
+// var a;
+// name='sri'
+// console.log('name:',name)
+
+// let a=7;
+// c=++a + a++ - --a + a--
+// console.log(c)
+
+let a=11;
+c=++a - --a + ++a - --a + ++a
+console.log(c)
