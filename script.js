1802ddb36fa64af341d7b2b226939549b76a6a09
Srinath08-sri <johnsrinath45@gmail.com>
2026-01-16 11:59:34 +0530

    updated files

--- script.js
+++ script.js
@@ -19,6 +19,88 @@
 // c=++a + a++ - --a + a--
 // console.log(c)
 
-let a=11;
-c=++a - --a + ++a - --a + ++a
-console.log(c)
+// let a=11;
+// c=++a - --a + ++a - --a + ++a
+// console.log(c)
+
+// let a=null
+// console.log(a)
+
+// let user={
+//     name:"sri",
+//     age:18,
+//     address:"hosur",
+//     id:12
+// }
+
+// console.log(user)
+
+// let user1={
+//     name:"fanta",
+//     age:30,
+//     address:"kollegal"
+// }
+
+//  user .name="abi"
+//  console.log(user1)
+
+//  let user={
+//     name:"sri",
+//     age:10,
+//     address:"Bangalore",
+//     Biodata:{
+//         Maritalstatus:"unmarried",
+//         qualification:"BSC",
+//         DOB:"12/12/2005",
+//         professional:"engineer"
+
+//     }
+//  }
+//  console.log(user .Biodata)
+
+// let a=12
+// let b=14
+// let c=15
+
+// if(a>b && a>c)
+// {
+//     console.log("a is greater")
+// }
+// else if(b>a && b>c)
+// {
+//     console.log("b is graeter")
+// }
+// else{
+//  console.log("c is greater")
+// }
+
+// if(a>b){
+//     console.log("a is greater")
+// }
+// if(b>c){
+//   console.log("b is greater")
+// }
+// else{
+//     console.log("c is greater")
+// }
+
+// if(333>44)
+// {
+//     console.log("HLO good mrng")
+//     if(333<44)
+//         {
+//         console.log("to everone")
+//     }
+// }
+
+if(10>2)
+{
+    console.log("sri")
+}
+else if(19<12)
+{
+    console.log("fanta")
+}
+else(
+    console.log("error")
+)
