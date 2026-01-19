731656a7d5aa6a98b8e7d477b03df2870f1544ce
Srinath08-sri <johnsrinath45@gmail.com>
2026-01-19 18:02:55 +0530

    updated files

--- script.js
+++ script.js
@@ -1,106 +1,13 @@
-// name='sri'
-// id=12
-// collegename='SFS'
-// address='hosur'
-// age=0x12
-// qualification='Bsc'
-// console.log("name:",name)
-// console.log("id:",id)
-// console.log("collegename:",collegename)
-// console.log("address:",address)
-// console.log("age:",age)
-// console.log("qualification:",qualification)
+for(let i=1;i<=10;i++)
+ {
+    console.log(2*i);
+ }
 
-// var a;
-// name='sri'
-// console.log('name:',name)
-
-// let a=7;
-// c=++a + a++ - --a + a--
-// console.log(c)
-
-// let a=11;
-// c=++a - --a + ++a - --a + ++a
-// console.log(c)
-
-// let a=null
-// console.log(a)
-
-// let user={
-//     name:"sri",
-//     age:18,
-//     address:"hosur",
-//     id:12
-// }
-
-// console.log(user)
-
-// let user1={
-//     name:"fanta",
-//     age:30,
-//     address:"kollegal"
-// }
-
-//  user .name="abi"
-//  console.log(user1)
-
-//  let user={
-//     name:"sri",
-//     age:10,
-//     address:"Bangalore",
-//     Biodata:{
-//         Maritalstatus:"unmarried",
-//         qualification:"BSC",
-//         DOB:"12/12/2005",
-//         professional:"engineer"
-
-//     }
-//  }
-//  console.log(user .Biodata)
-
-// let a=12
-// let b=14
-// let c=15
-
-// if(a>b && a>c)
-// {
-//     console.log("a is greater")
-// }
-// else if(b>a && b>c)
-// {
-//     console.log("b is graeter")
-// }
-// else{
-//  console.log("c is greater")
-// }
-
-// if(a>b){
-//     console.log("a is greater")
-// }
-// if(b>c){
-//   console.log("b is greater")
-// }
-// else{
-//     console.log("c is greater")
-// }
-
-// if(333>44)
-// {
-//     console.log("HLO good mrng")
-//     if(333<44)
-//         {
-//         console.log("to everone")
-//     }
-// }
-
-if(10>2)
+for(let i=1;i<=20;i++)
 {
-    console.log("sri")
+    for(let j=1;j<=10;j++)
+    {
+        console.log(i*j);
+    }
+    console.log("---------------");
 }
-else if(19<12)
-{
-    console.log("fanta")
-}
-else(
-    console.log("error")
-)
